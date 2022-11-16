716ebf144ce3ebc8b49275d8167355f571698465
jeffer14 <j.galvez1@utp.edu.co>
2022-11-15 23:29:17 -0500

    Se agrego los formularios y modelos a los viveros

--- Vivero/forms.py
+++ Vivero/forms.py
@@ -1,9 +1,81 @@
 from django import forms
-
-# from betterforms.multiform import MultiModelForm
+#from betterforms.multiform import MultiModelForm 
 from django.contrib.auth.forms import AuthenticationForm
 from django.db.models import query_utils
 from django.db.models.query import QuerySet
 from Vivero.models import Departamento, Municipio, Vivero
 
-# Create form
+#Create form
+class FormularioDepartamento(forms.ModelForm):
+
+    class Meta:
+        model = Departamento
+        fields = ('nombre_departamento',)
+        widgets = {
+            'nombre_departamento': forms.Select(
+                attrs = {
+                    'class': 'form-control',
+                    'placeholder': 'Selecciona un departamento...'
+                    
+                }
+            )
+        }
+
+class FormularioMunicipio(forms.ModelForm):
+    
+    class Meta:
+        model = Municipio
+        fields = ('nombre_municipio',)
+        widgets = {
+            'nombre_municipio': forms.Select(
+                attrs = {
+                    'class': 'form-control',
+                    'placeholder': 'Selecciona un municipio...'
+                }
+            )
+        }
+
+class FormularioVivero(forms.ModelForm):
+
+    class Meta:
+        model = Vivero
+        fields = ('codigo', 'nombre_vivero','productor', 'departamento','municipio')
+        widgets = {
+            'departamento': forms.TextInput(
+                attrs = {
+                    'class': 'form-control',
+                    'placeholder': 'Ingrese un departamento'
+                }
+            ),
+            'municipio': forms.TextInput(
+                attrs = {
+                    'class': 'form-control',
+                    'placeholder': 'Ingrese un municipio'
+                }
+            ),
+            'productor': forms.Select(
+                attrs = {
+                    'class': 'form-control',
+                    'placeholder': 'Selecciona un productor...'
+                }
+            ),
+            'codigo': forms.TextInput(
+                attrs = {
+                    'class': 'form-control',
+                    'placeholder': 'Ingrese un codigo'
+                }
+            ),
+            'nombre_vivero': forms.TextInput(
+                attrs = {
+                    'class': 'form-control',
+                    'placeholder': 'Ingrese un nombre'
+                }
+            )
+        }
+
+class DepartamentoMunicipioVivero(forms.ModelForm):
+    form_classes = {
+        'departamento': FormularioDepartamento,
+        'municipio': FormularioMunicipio,
+        'vivero': FormularioVivero,
+    }
